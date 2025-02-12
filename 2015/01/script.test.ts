81c3f9adb65e34d4901520ce1c20ebe63bf71c04
justDaBuK <kevinjust87@gmail.com>
2025-02-12 19:02:53 +0100

    feat: 2015/01 part 2

--- 2015/01/script.test.ts
+++ 2015/01/script.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { part1 } from './script';
+import { part1, part2 } from './script';
 import { readFileSync } from 'fs';
 
 test.each([
@@ -12,11 +12,11 @@ test.each([
     ["))(", -1],
     [")))", -3],
     [")())())", -3],
-])('%s -> %i', (input, expected) => {
+])('part1: %s -> %i', (input, expected) => {
     expect(part1(input)).toBe(expected);
 });
 
-test('actual input', () => {
+test('part1: actual input', () => {
     const file = readFileSync('./2015/01/input.txt', 'utf-8');
 
     let floorInstructions = "";
@@ -32,4 +32,29 @@ test('actual input', () => {
 
     const resultingFloor = part1(floorInstructions);
     expect(resultingFloor).toBe(232);
+})
+
+test.each([
+    [")", 1],
+    ["()())", 5],
+])('part2: %s -> %i', (input, expected) => {
+    expect(part2(input)).toBe(expected);
+});
+
+test('part2: actual input', () => {
+    const file = readFileSync('./2015/01/input.txt', 'utf-8');
+
+    let floorInstructions = "";
+
+    file.split(/\r?\n/).forEach((line: string) => {
+        const trimmedLine = line.trim();
+        if (!trimmedLine) {
+            console.log('reached the end');
+        } else {
+            floorInstructions += trimmedLine;
+        }
+    });
+
+    const enterBasementInstruction = part2(floorInstructions);
+    expect(enterBasementInstruction).toBe(1783);
 })
--- 2015/01/script.ts
+++ 2015/01/script.ts
@@ -5,3 +5,18 @@ export function part1(input: string) {
     floorInstructions.forEach(instruction => instruction === '(' ? floor++ : floor--);
     return floor;
 }
+
+export function part2(input: string) {
+    const floorInstructions = input.split('');
+    let floor = 0;
+    for(let index = 0; index < floorInstructions.length; index++) {
+        const instruction = floorInstructions[index];
+        floor += instruction === '(' ? 1 : -1;
+        if (floor === -1) {
+            return index + 1;
+        }
+    }
+    throw "Santa never enters the basement";
+}
+
+
