7fa657a1fc22abceb200dafa0fedd8e29edb9926
justDaBuK <kevinjust87@gmail.com>
2025-02-12 18:55:58 +0100

    feat: 2015/01 part 1

--- 2015/01/script.test.ts
+++ 2015/01/script.test.ts
@@ -1,6 +1,35 @@
 import { expect, test } from 'vitest';
 import { part1 } from './script';
+import { readFileSync } from 'fs';
 
-test('part1', () => {
-    expect(part1()).toBe(1);
+test.each([
+    ["(())", 0],
+    ["()()", 0],
+    ["(((", 3],
+    ["(()(()(", 3],
+    ["))(((((", 3],
+    ["())", -1],
+    ["))(", -1],
+    [")))", -3],
+    [")())())", -3],
+])('%s -> %i', (input, expected) => {
+    expect(part1(input)).toBe(expected);
+});
+
+test('actual input', () => {
+    const file = readFileSync('./2015/01/input.txt', 'utf-8');
+
+    let floorInstructions = "";
+
+    file.split(/\r?\n/).forEach((line: string) => {
+        const trimmedLine = line.trim();
+        if (!trimmedLine) {
+            console.log('reached the end');
+        } else {
+            floorInstructions += trimmedLine;
+        }
+    });
+
+    const resultingFloor = part1(floorInstructions);
+    expect(resultingFloor).toBe(232);
 })
--- 2015/01/script.ts
+++ 2015/01/script.ts
@@ -1,29 +1,7 @@
-import { readFileSync } from 'fs';
 
-function getData(fileName: string): number[][] {
-    const file = readFileSync(fileName, 'utf-8');
-
-    const numberListList: number[][] = [[],[]]
-
-    file.split(/\r?\n/).forEach((line: string) => {
-        const trimmedLine = line.trim();
-        if (!trimmedLine) {
-            console.log('reached the end');
-        } else {
-            const extractNumberRegex = /(\d+)/g;
-            let regExpMatchArray = trimmedLine.match(extractNumberRegex)
-            if(regExpMatchArray) {
-                regExpMatchArray.forEach((number, index) => {
-                    numberListList[index].push(parseInt(number));
-                });
-            }
-        }
-    });
-
-    return numberListList;
-}
-
-export function part1() {
-    console.log("I did something");
-    return 1;
+export function part1(input: string) {
+    const floorInstructions = input.split('');
+    let floor = 0;
+    floorInstructions.forEach(instruction => instruction === '(' ? floor++ : floor--);
+    return floor;
 }
