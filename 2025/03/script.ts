869dee9f91e5985db6ac5dcfe3ebe5e84f5917d8
Kevin Just <kevin.just@agile-robots.com>
2025-12-04 17:43:10 +0100

    feat: preperation

--- 2025/03/script.ts
+++ 2025/03/script.ts
@@ -45,7 +45,7 @@ function part1() {
   console.log("jultageSum", jultageSum);
 }
 
-const debug = false;
+const debug = true;
 function findMaxJoltage12Batteries(bank: number[]): number {
   if (debug) console.group(bank.join(""));
   let maxJoltage = 0;
@@ -60,121 +60,19 @@ function findMaxJoltage12Batteries(bank: number[]): number {
     }
   };
 
-  // no the moving frame is wrong, as there can also be solutions that have gaps in between
-  let initialIndexList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-  let indexList = [...initialIndexList];
+  const windowSize = 12;
 
-  while (indexList[0] < bank.length - 11) {
-    while (indexList[1] < bank.length - 10) {
-      while (indexList[2] < bank.length - 9) {
-        while (indexList[3] < bank.length - 8) {
-          while (indexList[4] < bank.length - 7) {
-            while (indexList[5] < bank.length - 6) {
-              while (indexList[6] < bank.length - 5) {
-                while (indexList[7] < bank.length - 4) {
-                  while (indexList[8] < bank.length - 3) {
-                    while (indexList[9] < bank.length - 2) {
-                      while (indexList[10] < bank.length - 1) {
-                        while (indexList[11] < bank.length) {
-                          checkAndReplaceJoltage(indexList);
-                          indexList[11]++;
-                        }
-                        indexList[10]++;
-                        indexList[11] = indexList[10] + 1;
-                      }
-                      indexList[9]++;
-                      indexList[10] = indexList[9] + 1;
-                      indexList[11] = indexList[10] + 1;
-                    }
-                    indexList[8]++;
-                    indexList[9] = indexList[8] + 1;
-                    indexList[10] = indexList[9] + 1;
-                    indexList[11] = indexList[10] + 1;
-                  }
-                  indexList[7]++;
-                  indexList[8] = indexList[7] + 1;
-                  indexList[9] = indexList[8] + 1;
-                  indexList[10] = indexList[9] + 1;
-                  indexList[11] = indexList[10] + 1;
-                }
-                indexList[6]++;
-                indexList[7] = indexList[6] + 1;
-                indexList[8] = indexList[7] + 1;
-                indexList[9] = indexList[8] + 1;
-                indexList[10] = indexList[9] + 1;
-                indexList[11] = indexList[10] + 1;
-              }
-              indexList[5]++;
-              indexList[6] = indexList[5] + 1;
-              indexList[7] = indexList[6] + 1;
-              indexList[8] = indexList[7] + 1;
-              indexList[9] = indexList[8] + 1;
-              indexList[10] = indexList[9] + 1;
-              indexList[11] = indexList[10] + 1;
-            }
-            indexList[4]++;
-            indexList[5] = indexList[4] + 1;
-            indexList[6] = indexList[5] + 1;
-            indexList[7] = indexList[6] + 1;
-            indexList[8] = indexList[7] + 1;
-            indexList[9] = indexList[8] + 1;
-            indexList[10] = indexList[9] + 1;
-            indexList[11] = indexList[10] + 1;
-          }
-          indexList[3]++;
-          indexList[4] = indexList[3] + 1;
-          indexList[5] = indexList[4] + 1;
-          indexList[6] = indexList[5] + 1;
-          indexList[7] = indexList[6] + 1;
-          indexList[8] = indexList[7] + 1;
-          indexList[9] = indexList[8] + 1;
-          indexList[10] = indexList[9] + 1;
-          indexList[11] = indexList[10] + 1;
-        }
-        indexList[2]++;
-        indexList[3] = indexList[2] + 1;
-        indexList[4] = indexList[3] + 1;
-        indexList[5] = indexList[4] + 1;
-        indexList[6] = indexList[5] + 1;
-        indexList[7] = indexList[6] + 1;
-        indexList[8] = indexList[7] + 1;
-        indexList[9] = indexList[8] + 1;
-        indexList[10] = indexList[9] + 1;
-        indexList[11] = indexList[10] + 1;
-      }
-      indexList[1]++;
-      indexList[2] = indexList[1] + 1;
-      indexList[3] = indexList[2] + 1;
-      indexList[4] = indexList[3] + 1;
-      indexList[5] = indexList[4] + 1;
-      indexList[6] = indexList[5] + 1;
-      indexList[7] = indexList[6] + 1;
-      indexList[8] = indexList[7] + 1;
-      indexList[9] = indexList[8] + 1;
-      indexList[10] = indexList[9] + 1;
-      indexList[11] = indexList[10] + 1;
-    }
-    indexList[0]++;
-    indexList[1] = indexList[0] + 1;
-    indexList[2] = indexList[1] + 1;
-    indexList[3] = indexList[2] + 1;
-    indexList[4] = indexList[3] + 1;
-    indexList[5] = indexList[4] + 1;
-    indexList[6] = indexList[5] + 1;
-    indexList[7] = indexList[6] + 1;
-    indexList[8] = indexList[7] + 1;
-    indexList[9] = indexList[8] + 1;
-    indexList[10] = indexList[9] + 1;
-    indexList[11] = indexList[10] + 1;
+  let currentHighestNumberList: number[] = [];
+  for (let i = 0; i < windowSize; i++) {
+    currentHighestNumberList.push(9);
   }
-
-  if (debug) console.log(maxJoltage);
+  currentHighestNumberList.map((num) => )
   if (debug) console.groupEnd();
   return maxJoltage;
 }
 
 function part2() {
-  const bankList = getData("./input.txt");
+  const bankList = getData("./test-input.txt");
   const maxJoltageList = bankList.map((bank, index) => {
     if (index % 10 === 0)
       console.log(`Processing bank ${index + 1} of ${bankList.length}`);
